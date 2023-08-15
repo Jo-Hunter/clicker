8755926268d5d148e5538ec15d3fe674a17c9fda
Jocelyn Hunter <jocelyn.tech.services@gmail.com>
2023-08-15 23:01:01 +1000

    add bank record count functionality

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
+let bankEl = document.getElementById("bank-el")
 let count = 0
+let bankRecord = 0
 
 function increment() {
     count += 1
     countEl.textContent = count
+    if(count > bankRecord){
+        bankRecord = count
+    }
 }
 
 function save() {
@@ -12,4 +17,7 @@ function save() {
     saveEl.textContent += countStr
     countEl.textContent = 0
     count = 0
+    bankEl.textContent = bankRecord
 }
+
+
